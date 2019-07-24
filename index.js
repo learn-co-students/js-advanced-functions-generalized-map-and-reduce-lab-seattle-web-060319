a521b3c0241bbd589d6c04ef3c203563cf6ede26
kargo <karenrgo@gmail.com>
2019-07-24 14:38:21 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Add your functions here
+function map(arr, blk) {
+    return arr.map(blk)
+}
+
+function reduce(arr, fx, start) {
+    if (!!start == false) {
+        start = arr.shift()
+    } else {
+        start = start
+    }
+    return arr.reduce(fx, start)
+}
