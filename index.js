ca6885a7cf5975c90155af7465eaa0eee0891236
Spencer Lindemuth <lindemut@gmail.com>
2019-07-24 17:00:58 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Add your functions here
+let map = (array, func) => {
+    return array.map(element => func(element))
+}
+
+let reduce = (array, func, starting_point) => {   
+    if(!starting_point){
+        starting_point = array[0]
+        array.shift()
+    }
+    return array.reduce(func, starting_point)
+}
